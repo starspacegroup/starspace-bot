659aba099b50916474d0d8dbd1e1466ffed3082b
davis9001 <monaghan.david@gmail.com>
2023-07-29 05:10:13 -0700

    fix for typescript

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,7 +1,6 @@
-const { REST, Routes } = require("discord.js")
-// const { clientId, guildId, token } = require("./config.json")
-const fs = require("node:fs")
-const path = require("node:path")
+import { REST, Routes } from "discord.js"
+import fs from "node:fs"
+import path from "node:path"
 import * as dotenv from "dotenv"
 dotenv.config()
 
@@ -14,6 +13,8 @@ const commands: string[] = []
 const foldersPath = path.join(__dirname, "commands")
 const commandFolders = fs.readdirSync(foldersPath)
 
+// const _commands = [setJoinTime, someOtherCommand, yetAnotherCommand].map(command => command.toJSON())
+
 for (const folder of commandFolders) {
   // Grab all the command files from the commands directory you created earlier
   const commandsPath = path.join(foldersPath, folder)
@@ -21,6 +22,7 @@ for (const folder of commandFolders) {
     .readdirSync(commandsPath)
     .filter((file) => file.endsWith(".js"))
   // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file)
     const command = require(filePath)
@@ -35,6 +37,7 @@ for (const folder of commandFolders) {
 }
 
 // Construct and prepare an instance of the REST module
+// @ts-ignore
 const rest = new REST().setToken(token)
 
 // and deploy your commands!
@@ -46,11 +49,13 @@ const rest = new REST().setToken(token)
 
     // The put method is used to fully refresh all commands in the guild with the current set
     const data = await rest.put(
+      // @ts-ignore
       Routes.applicationGuildCommands(clientId, guildId),
       { body: commands }
     )
 
     console.log(
+      // @ts-ignore
       `Successfully reloaded ${data.length} application (/) commands.`
     )
   } catch (error) {
