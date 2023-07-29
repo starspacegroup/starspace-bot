0e7ce08689866dc1f1976aeed8e9c611264dafd1
davis9001 <monaghan.david@gmail.com>
2023-07-29 05:13:19 -0700

    TS fix and stay connected to mongo

--- src/connections/mongoDb.ts
+++ src/connections/mongoDb.ts
@@ -1,28 +1,21 @@
-const { MongoClient, ServerApiVersion } = require("mongodb")
+import { MongoClient, ServerApiVersion } from "mongodb"
 const mongoUser = process.env.MONGO_USER
 const mongoPass = process.env.MONGO_PASS
 const mongoDb = process.env.MONGO_DB
 const uri = `mongodb+srv://${mongoUser}:${mongoPass}@cameraon.ihn5vri.mongodb.net/${mongoDb}?retryWrites=true&w=majority`
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+
+const mongoClient = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   },
 })
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect()
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 })
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    )
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close()
-  }
-}
-run().catch(console.dir)
+
+process.on("SIGINT", async () => {
+  await mongoClient.close()
+})
+process.on("SIGTERM", async () => {
+  await mongoClient.close()
+})
+export default mongoClient
