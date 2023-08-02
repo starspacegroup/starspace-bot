3c0f4a3c078afa48a33df4f77f4a541543588c43
davis9001 <monaghan.david@gmail.com>
2023-08-02 10:14:43 -0700

    event enum

--- src/models/voiceChannelEvent.ts
+++ src/models/voiceChannelEvent.ts
@@ -4,8 +4,15 @@ export default class VoiceChannelEvent {
   constructor(
     public memberId: string,
     public channel: string,
-    public cameraEnabled: boolean,
+    public action: VoiceChannelAction,
     public timestamp: Date,
     public id?: ObjectId
   ) {}
+}
+
+export enum VoiceChannelAction {
+  Join = "join",
+  Leave = "leave",
+  Kick = "kick",
+  CameraOn = "camera_on",
 }
