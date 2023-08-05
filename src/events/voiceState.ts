3da3f17dab5961222be7442e3e587ed505f077fe
davis9001 <monaghan.david@gmail.com>
2023-08-05 11:29:10 -0700

    clean up working event saves

--- src/events/voiceState.ts
+++ src/events/voiceState.ts
@@ -1,6 +1,5 @@
 import { getJoinTime, insertVoiceChannelEvent } from "../connections/mongoDb"
-import TimeSetting from "../models/timeSetting"
-import ViuceChannelEvent from "../models/voiceChannelEvent"
+import mongoClient from "../connections/mongoDb"
 
 import {
   Events,
@@ -9,6 +8,7 @@ import {
   VoiceState,
   Channel,
   VoiceBasedChannel,
+  GuildMember,
 } from "discord.js"
 import {
   getVoiceConnection,
@@ -18,6 +18,7 @@ import {
   AudioPlayerStatus,
 } from "@discordjs/voice"
 import { checkPrimeSync } from "crypto"
+import { Agenda } from "@hokify/agenda"
 
 const intents = [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
 
@@ -31,7 +32,14 @@ const intents = [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
 //   }
 // })
 
-const memberMoved = (member, oldState, newState) => {
+// @ts-ignore
+// const agenda = new Agenda({ mongo: mongoClient })
+
+const memberMoved = (
+  member: GuildMember,
+  oldState: VoiceState,
+  newState: VoiceState
+) => {
   const oldChannel = oldState.channel
   const newChannel = newState.channel
   if (oldState.channel !== newState.channel) {
@@ -43,7 +51,11 @@ const memberMoved = (member, oldState, newState) => {
   }
 }
 
-const memberJoined = (member, oldState, newState) => {
+const memberJoined = (
+  member: GuildMember,
+  oldState: VoiceState,
+  newState: VoiceState
+) => {
   const oldChannel = oldState.channel
   const newChannel = newState.channel
   if (oldState.channel !== newState.channel) {
@@ -56,7 +68,11 @@ const memberJoined = (member, oldState, newState) => {
   return false
 }
 
-const memberLeft = (member, oldState, newState) => {
+const memberLeft = (
+  member: GuildMember,
+  oldState: VoiceState,
+  newState: VoiceState
+) => {
   const oldChannel = oldState.channel
   const newChannel = newState.channel
   if (oldState.channel !== newState.channel) {
@@ -69,8 +85,11 @@ const memberLeft = (member, oldState, newState) => {
   return false
 }
 
-const cameraDisabled = (member, oldState, newState) => {
-  const oldChannel = oldState.channel
+const cameraDisabled = (
+  member: GuildMember,
+  oldState: VoiceState,
+  newState: VoiceState
+) => {
   const newChannel = newState.channel
   const oldCamState = oldState.selfVideo ? "on" : "off"
   const newCamState = newState.selfVideo ? "on" : "off"
@@ -84,8 +103,11 @@ const cameraDisabled = (member, oldState, newState) => {
   return false
 }
 
-const cameraEnabled = (member, oldState, newState) => {
-  const oldChannel = oldState.channel
+const cameraEnabled = (
+  member: GuildMember,
+  oldState: VoiceState,
+  newState: VoiceState
+) => {
   const newChannel = newState.channel
   const oldCamState = oldState.selfVideo ? "on" : "off"
   const newCamState = newState.selfVideo ? "on" : "off"
@@ -102,46 +124,37 @@ const cameraEnabled = (member, oldState, newState) => {
 export function voiceStateEvent(oldState: VoiceState, newState: VoiceState) {
   const member = newState.member
   const userName = member?.user.tag
-  const oldChannel = oldState.channel
-  const newChannel = newState.channel
-  const oldCamState = oldState.selfVideo ? "on" : "off"
-  const newCamState = newState.selfVideo ? "on" : "off"
-  const bot = newState.client
+  if (member) {
+    if (memberJoined(member, oldState, newState) && newState.channel) {
+      console.log(`${userName} joined ${newState.channel?.name}.`)
+      insertVoiceChannelEvent(member, newState.channel, "join")
+    }
 
-  if (memberJoined(member, oldState, newState) && member && newState.channel) {
-    console.log(`${userName} joined ${newState.channel?.name}.`)
-    insertVoiceChannelEvent(member, newState.channel, "join")
-  }
+    if (memberLeft(member, oldState, newState) && oldState.channel) {
+      console.log(`${userName} left ${oldState.channel?.name}.`)
+      insertVoiceChannelEvent(member, oldState.channel, "leave")
+    }
 
-  if (memberLeft(member, oldState, newState) && member && oldState.channel) {
-    console.log(`${userName} left ${oldState.channel?.name}.`)
-    insertVoiceChannelEvent(member, oldState.channel, "leave")
-  }
+    if (
+      memberMoved(member, oldState, newState) &&
+      newState.channel &&
+      oldState.channel
+    ) {
+      console.log(
+        `${userName} moved from ${oldState.channel?.name} to ${newState.channel?.name}.`
+      )
+      insertVoiceChannelEvent(member, oldState.channel, "leave")
+      insertVoiceChannelEvent(member, newState.channel, "join")
+    }
 
-  if (
-    memberMoved(member, oldState, newState) &&
-    member &&
-    newState.channel &&
-    oldState.channel
-  ) {
-    console.log(
-      `${userName} moved from ${oldState.channel?.name} to ${newState.channel?.name}.`
-    )
-    insertVoiceChannelEvent(member, oldState.channel, "leave")
-    insertVoiceChannelEvent(member, newState.channel, "join")
-  }
+    if (cameraDisabled(member, oldState, newState) && newState.channel) {
+      console.log(`${userName} camera disabled.`)
+      insertVoiceChannelEvent(member, newState.channel, "cameraOff")
+    }
 
-  if (
-    cameraDisabled(member, oldState, newState) &&
-    member &&
-    newState.channel
-  ) {
-    console.log(`${userName} camera disabled.`)
-    insertVoiceChannelEvent(member, newState.channel, "cameraOff")
-  }
-
-  if (cameraEnabled(member, oldState, newState) && member && newState.channel) {
-    console.log(`${userName} camera enabled.`)
-    insertVoiceChannelEvent(member, newState.channel, "cameraOn")
+    if (cameraEnabled(member, oldState, newState) && newState.channel) {
+      console.log(`${userName} camera enabled.`)
+      insertVoiceChannelEvent(member, newState.channel, "cameraOn")
+    }
   }
 }
