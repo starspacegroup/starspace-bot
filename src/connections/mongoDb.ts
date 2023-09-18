af2cedfcd37188e71cc995061b1ac1813aff030b
davis9001 <monaghan.david@gmail.com>
2023-09-18 13:48:53 -0700

    Kill some disconnect bugs

--- src/connections/mongoDb.ts
+++ src/connections/mongoDb.ts
@@ -8,7 +8,7 @@ import log from "../lib/logger"
 const mongoUser = process.env.MONGO_USER
 const mongoPass = process.env.MONGO_PASS
 const mongoDb = process.env.MONGO_DB
-const uri = `mongodb+srv://${mongoUser}:${mongoPass}@cameraon.ihn5vri.mongodb.net/${mongoDb}?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${mongoUser}:${mongoPass}@cameraon.ihn5vri.mongodb.net/${mongoDb}?retryWrites=false&w=majority`
 
 const mongoClient = new MongoClient(uri, {
   serverApi: {
@@ -54,10 +54,14 @@ export const getJoinTime = async () => {
   return result.value
 }
 
-process.on("SIGINT", async () => {
-  await mongoClient.close()
+process.on("SIGINT", () => {
+  mongoClient.close().then(() => {
+    process.exit(0)
+  })
 })
-process.on("SIGTERM", async () => {
-  await mongoClient.close()
+process.on("SIGTERM", () => {
+  mongoClient.close().then(() => {
+    process.exit(0)
+  })
 })
 export default mongoClient
