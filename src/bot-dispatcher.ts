77d71ee05135156d4fe66152e8aea814b9614dc8
davis9001 <monaghan.david@gmail.com>
2024-01-03 07:47:16 -0700

    cleanup and remove schedule shit

--- src/bot-dispatcher.ts
+++ src/bot-dispatcher.ts
@@ -2,12 +2,6 @@ import log, { lerror } from "./lib/logger"
 import * as dotenv from "dotenv"
 dotenv.config()
 const botToken = process.env.DISCORD_BOT_TOKEN
-const mongoUser = process.env.MONGO_USER
-const mongoPass = process.env.MONGO_PASS
-const mongoDb = process.env.MONGO_DB
-const mongoUrl = process.env.MONGO_URL
-
-import { Agenda } from "@hokify/agenda"
 
 import mongoClient, {
   getMutedRole,
@@ -23,16 +17,9 @@ import {
   GuildMember,
 } from "discord.js"
 import { ChangeStreamInsertDocument } from "mongodb"
-import { NumberSettingType } from "./models/numberSetting"
 let currentMemberBeingWarned = []
 
 const database = mongoClient.db("camera_on")
-const agenda = new Agenda({
-  db: {
-    address: `mongodb+srv://${mongoUser}:${mongoPass}@${mongoUrl}/${mongoDb}?retryWrites=true&w=majority`,
-  },
-  processEvery: "15ms",
-})
 const discordClient = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
 })
@@ -40,30 +27,8 @@ discordClient.login(botToken)
 
 type DesiredAction = "turn on camera" | "stop screen sharing" | "leave"
 
-const createAgendaJobs = async () => {
-  agenda.define("disconnect member", async (job) => {
-    const guild = discordClient.guilds.cache.get(job.attrs.data.guildId)
-    const channel = await discordClient.channels.fetch(job.attrs.data.channelId)
-    const member = guild?.members.cache.get(job.attrs.data.memberId)
-    const voiceChannel = channel?.isVoiceBased() ? channel : null
-    if (!guild || !voiceChannel || !member) return
-    try {
-      disconnectMember(guild, voiceChannel, member)
-    } catch (e) {
-      lerror(e)
-    }
-  })
-}
-
-class MyEvent {
-  should_handle!: boolean
-  desired_action!: string
-}
-
 export const botScheduler = {
   async run() {
-    await createAgendaJobs()
-    await agenda.start()
     const voiceChannelEvents =
       database.collection<VoiceChannelEvent>("voiceChannelEvent")
 
@@ -92,18 +57,6 @@ export const botScheduler = {
           log(`${guild.name}: Bot is not enabled on server.`)
           return
         }
-        // const botGuildMember = await channel?.guild.members.fetch(botUser)
-        // if (
-        //   !channel
-        //     .permissionsFor(botGuildMember)
-        //     .has(PermissionsBitField.Flags.Connect) ||
-        //   !channel
-        //     .permissionsFor(botGuildMember)
-        //     .has(PermissionsBitField.Flags.SendMessages)
-        // ) {
-        //   // ignore channels the bot can't join or send messages in
-        //   return
-        // }
         switch (action) {
           case "join":
           case "cameraOff":
@@ -235,60 +188,4 @@ const disconnectMember = (
   } catch (error) {
     lerror(error)
   }
-}
-
-const cancelJob = async (
-  name: string,
-  guild: Guild,
-  channel: VoiceBasedChannel,
-  member: GuildMember
-) => {
-  const numJobsRemoved = await agenda
-    .cancel({
-      name: name,
-      "data.guildId": guild.id,
-      "data.channelId": channel.id,
-      "data.memberId": member.id,
-    })
-    .catch((error) => {
-      lerror(error)
-    })
-  agenda.jobs()
-  log(
-    `Removed ${numJobsRemoved} jobs for ${member.user.tag} in ${channel.name}.`
-  )
-}
-
-interface JobAttributes {
-  name: string
-  memberId: string
-  channelId: string
-  guildId: string
-  desiredAction: DesiredAction
-}
-
-function scheduleJob(
-  name: string,
-  delayInSeconds: number,
-  guild: Guild,
-  channel: VoiceBasedChannel,
-  member: GuildMember,
-  desiredAction: DesiredAction
-) {
-  log(
-    `Scheduling job in ${guild.name} in ${channel.name} for ${member.user.tag} in ${delayInSeconds} seconds.`
-  )
-  const jobAttributes: JobAttributes = {
-    name: name,
-    memberId: member.id,
-    channelId: channel.id,
-    guildId: guild.id,
-    desiredAction: desiredAction,
-  }
-  const job = agenda.create(name, jobAttributes)
-  job.schedule(`in ${delayInSeconds} seconds`)
-  job.save().catch((error) => {
-    lerror(error)
-  })
-  return job.attrs._id
 }
