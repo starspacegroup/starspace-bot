20284467778920d03ae5ca1f16892eba4a8edbeb
davis9001 <monaghan.david@gmail.com>
2024-01-18 07:14:29 -0700

    Don't need date in log on production (using systemd)

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,8 +1,19 @@
-export default function log(message) {
+import { env } from "process"
+
+export default function log(message: any) {
   const timestamp = new Date()
-  console.log(`[${timestamp.toUTCString()}] ${message}`)
+  if (env.NODE_ENV !== "production") {
+    console.log(`[${timestamp.toUTCString()}] ${message}`)
+    return
+  }
+  console.log(`${message}`)
 }
-export function lerror(message) {
+
+export function lerror(message: any) {
   const timestamp = new Date()
-  console.error(`[${timestamp.toUTCString()}] ERROR: ${message}`)
+  if (env.NODE_ENV !== "production") {
+    console.error(`[${timestamp.toUTCString()}] ERROR: ${message}`)
+    return
+  }
+  console.error(`ERROR: ${message}`)
 }
