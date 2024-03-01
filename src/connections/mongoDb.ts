da292e078b17668fb5ce2aae27c214922f83a8b9
davis9001 <monaghan.david@gmail.com>
2024-02-29 21:55:53 -0700

    Use env variable for db url

--- src/connections/mongoDb.ts
+++ src/connections/mongoDb.ts
@@ -11,7 +11,8 @@ import MutedRole from "../models/mutedRole"
 const mongoUser = process.env.MONGO_USER
 const mongoPass = process.env.MONGO_PASS
 const mongoDb = process.env.MONGO_DB
-const uri = `mongodb+srv://${mongoUser}:${mongoPass}@cameraon.ihn5vri.mongodb.net/${mongoDb}?retryWrites=false&w=majority`
+const mongoUrl = process.env.MONGO_URL
+const uri = `mongodb+srv://${mongoUser}:${mongoPass}@${mongoUrl}/${mongoDb}?retryWrites=false&w=majority`
 
 const mongoClient = new MongoClient(uri, {
   serverApi: {
